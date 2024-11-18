4c46f2f6b8715a10e5f306fed7cd61e0f8ce5365
fatmaesprit2023 <125260896+fatmaesprit2023@users.noreply.github.com>
2024-11-18 14:29:12 +0100

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,41 @@
-fun main(args: Array<String>) {
-    println("Hello World!!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main(args: Array<String>) {
+    when (args.size) {
+        0 -> {
+            // Aucun argument, boucle pour attendre l'entrée utilisateur
+            while (true) {
+                print("Entrez quelque chose (ou tapez 'exit' pour quitter) : ")
+                val input = readLine()
+                if (input.equals("exit", ignoreCase = true)) {
+                    println("Au revoir !")
+                    break
+                }
+                println("Vous avez entré : $input")
+            }
+        }
+        1 -> {
+            // Un seul argument, lire le fichier spécifié
+            val fileName = args[0]
+            try {
+                val content = File(fileName).readText()
+                println("Contenu du fichier \"$fileName\" :")
+                println(content)
+            } catch (e: Exception) {
+                println("Erreur : Impossible de lire le fichier \"$fileName\".")
+                println("Détails : ${e.message}")
+            }
+        }
+        else -> {
+            // Plus d'un argument, afficher l'aide
+            afficherAide()
+        }
+    }
+}
+
+fun afficherAide() {
+    println("Usage :")
+    println("  - Sans argument : le programme attend des entrées utilisateur, les affiche et recommence.")
+    println("  - Avec un seul argument : le programme lit le fichier spécifié et affiche son contenu.")
+    println("  - Avec plusieurs arguments : affiche cette aide.")
 }
